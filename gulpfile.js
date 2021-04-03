58ae8248b53f78494dff75fa303b79e9d6e94ba0
makoto_kw <makoto.kw@gmail.com>
2021-04-04 07:31:20 +0900

    ruby-sass and Compass -> node-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,46 @@
 'use strict';
 
 var gulp = require('gulp');
-var gulpCompass = require('gulp-compass');
+var gulpSass = require('gulp-sass');
 var gulpAutoPrefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
-function compass(env) {
-    return gulp.src('./sass/*.scss')
-        .pipe(gulpCompass({
-            config_file: 'sass/config.rb',
-            environment: env,
-            comments: env === 'development',
-            style: env === 'development' ? 'expanded' : 'compact',
-            css: 'stylesheets',
-            sass: 'sass',
-            force: env !== 'development'
-        }))
-        .on('error', function (err) {
-            console.log(err);
-        })
-        .pipe(gulpAutoPrefixer({
-            cascade: false
-        }))
-        .pipe(gulp.dest('stylesheets'))
-        .pipe(browserSync.stream())
+gulpSass.compiler = require('node-sass');
+
+function sass(env) {
+  var nodeSassOption = {
+    outputStyle: 'expanded',
+    sourceComments: env === 'development'
+  };
+  // sourceComments
+  return gulp.src('./sass/*.scss')
+    .pipe(gulpSass(nodeSassOption).on('error', gulpSass.logError))
+    .pipe(gulpAutoPrefixer({
+      cascade: true
+    }))
+    .pipe(gulp.dest('stylesheets'))
+    .pipe(browserSync.stream())
     ;
 }
 
-gulp.task('compass:prod', function () {
-    return compass('production');
+gulp.task('sass:prod', function () {
+  return sass('production');
 });
 
-gulp.task('compass:dev', function () {
-    return compass('development');
+gulp.task('sass:dev', function () {
+  return sass('development');
 });
 
 gulp.task('browser-sync', function (done) {
-    browserSync.init({
-        port: 3001,
-        proxy: '127.0.0.1:3000'
-    });
-    done();
+  browserSync.init({
+    port: 3001,
+    proxy: '127.0.0.1:3000'
+  });
+  done();
 });
 
-gulp.task('debug', gulp.series('compass:dev', 'browser-sync', function () {
-    gulp.watch('sass/**/*.scss', gulp.task('compass:dev'));
+gulp.task('debug', gulp.series('sass:dev', 'browser-sync', function () {
+  gulp.watch('sass/**/*.scss', gulp.task('sass:dev'));
 }));
 
-gulp.task('default', gulp.series('compass:prod'));
+gulp.task('default', gulp.series('sass:prod'));
