b5d94e5edfc6c10bb54b6e6367f24a57f6176ee2
makoto_kw <makoto.kw@gmail.com>
2025-08-22 09:07:49 +0900

    Migrate Sass logic from deprecated functions to `sass:color` module and silence @import deprecation warnings in Vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,23 @@
 import { defineConfig } from 'vite';
 import path from 'path';
+import fs from 'fs';
 
 // Vite config to compile Sass to stylesheets/application.css
 export default defineConfig(({ command, mode }) => {
   const isDev = mode === 'development' || command === 'serve';
+
+  const headerContent = fs.readFileSync(path.resolve(__dirname, 'header.css'), 'utf-8');
+
   return {
-    // We are not serving an index.html. Use build/watch instead for dev.
+    // We are not serving an index.html. Use build/watch instead of dev.
+    // https://v5.vite.dev/config/build-options.html
     build: {
       outDir: 'stylesheets',
       assetsDir: '',
       emptyOutDir: false,
       sourcemap: isDev,
       cssCodeSplit: false,
+      cssMinify: false,
       rollupOptions: {
         input: path.resolve(__dirname, 'sass/application.scss'),
         output: {
@@ -22,12 +28,32 @@ export default defineConfig(({ command, mode }) => {
             }
             return '[name][extname]';
           }
-        }
+        },
+        plugins: [
+          {
+            name: 'theme-header',
+            generateBundle(options, bundle) {
+              Object.keys(bundle).forEach(fileName => {
+                const file = bundle[fileName];
+                if (fileName === 'application.css' && file.type === 'asset') {
+                  file.source = headerContent + file.source;
+                }
+              });
+            }
+          }
+        ]
       }
     },
     css: {
       devSourcemap: true,
-      postcss: './postcss.config.js'
+      postcss: './postcss.config.js',
+      preprocessorOptions: {
+        scss: {
+          // Silence only the @import deprecation warnings to avoid noisy output
+          // until we fully migrate to @use/@forward.
+          silenceDeprecations: ['import']
+        }
+      }
     },
     // Note: Vite dev server is not used here because Redmine serves HTML.
     // If you still want to try, you can run `vite` and integrate the client in your pages.
